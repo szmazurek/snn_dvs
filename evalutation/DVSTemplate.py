186874a9338ad4a2b4ad63718e1840b912c21c42
krzysjed <58859784+krzysjed@users.noreply.github.com>
2023-08-16 18:03:19 +0200

    Update DVSTemplate.py

--- evalutation/DVSTemplate.py
+++ evalutation/DVSTemplate.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 import wandb
 import os
 import glob
@@ -7,13 +9,14 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.transforms as transforms
 from torch.utils.data import Dataset, DataLoader
-from utils import train_val_dataset, f1_score, save_model
+from utils import train_val_dataset, save_model, f1_score as f1
 from spikingjelly.activation_based import functional
 from PIL import Image
 from typing import Tuple
 from torchvision.models import efficientnet_v2_s
 from spikingjelly.activation_based import ann2snn
 from models import SNN_1 as SNN
+
 
 
 class DvsDataset(Dataset):
@@ -51,13 +54,16 @@ class DvsDataset(Dataset):
         return images_tensor, label_tensor
 
 
+
+
+
 def main():
     wandb.init(
         project="SNN_1_DVS_Default",
         entity="snn_team"
     )
 
-    epochs = 500
+    epochs = 10000
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     checkpoint_file_save = "checkpoint_DVS_SNN1.pth"
     dataset = DvsDataset(r"/home/plgkrzysjed1/datasets/dataset_dvs")
@@ -79,42 +85,43 @@ def main():
     for epoch in range(0, epochs):
         net.train()
         train_loss = 0
-        train_acc = 0
         train_samples = 0
-        train_f1 = 0
-        i = 0
+        train_acc = 0
+        train_f1_list = 0
+        train_label_list = 0
+
         for img, label in train_data_loader:
             optimizer.zero_grad()
 
-            label = label.to(device)
-            label_shifted = torch.roll(label, shifts=0, dims=1)
-            label_onehot_shifted = F.one_hot(label_shifted, 2).float()
+            label = label.to(device).squeeze(0)
+            label_onehot = F.one_hot(label, 2).float()
 
             img = img.permute(1, 0, 2, 3, 4)  # [B, T, C, H, W] --> [T, B, C, H, W]
-            img =img.to(device).float()
+            img = img.to(device).float()
 
-            out_fr = net(img)  # [T, B, 2]
-            out_fr = out_fr.unsqueeze(0).permute(2, 1, 0, 3)  # [1, T, B, 2] --> [B, T, 1, 2]
-            pred = torch.argmax(out_fr, dim=3)
+            out_fr = net(img)  # [T, 1, 2]
 
             l = nn.MSELoss()
-
-            loss = l(out_fr, label_onehot_shifted.float())
-
+            loss = l(out_fr, label_onehot.float())
             loss.backward()
-            optimizer.step()
-
-            train_acc += (pred == label_shifted.int()).float().sum().item()
             train_samples += label.numel()
             train_loss += loss.item() * label.numel()
+            train_acc += (out_fr.argmax(2) == label).float().sum().item()
+            if isinstance(train_f1_list, int):
 
-            train_f1 += f1_score(pred, label_shifted.int())
-            i += 1
+                train_f1_list = out_fr.argmax(2).cpu()
+                train_label_list = label.cpu()
+            else:
+                train_f1_list = np.vstack((train_f1_list, out_fr.argmax(2).cpu()))
+                train_label_list = np.vstack((train_label_list, label.cpu()))
+
+            optimizer.step()
             functional.reset_net(net)
 
-        train_f1 /= i
         train_loss /= train_samples
         train_acc /= train_samples
+        train_f1 = f1(torch.Tensor(train_f1_list),torch.Tensor(train_label_list))
+
         print("Train", epoch, train_acc, train_f1)
 
         lr_scheduler.step()
@@ -123,38 +130,37 @@ def main():
         test_loss = 0
         test_acc = 0
         test_samples = 0
-        test_f1 = 0
-        i = 0
+        test_f1_list = 0
+        test_label_list = 0
         with torch.no_grad():
             for img, label in test_data_loader:
-                # img = img.to(device).float()
-
-                label = label.to(device)
-                label_shifted = torch.roll(label, shifts=0, dims=1)
-                label_onehot_shifted = F.one_hot(label_shifted, 2).float()
+                label = label.to(device).squeeze(0)
+                label_onehot = F.one_hot(label, 2).float()
 
                 img = img.permute(1, 0, 2, 3, 4)  # [B, T, C, H, W] --> [T, B, C, H, W]
                 img = img.to(device).float()
 
-                out_fr = net(img)  # [T, B, 2]
-                out_fr = out_fr.unsqueeze(0).permute(2, 1, 0, 3)  # [1, T, B, 2] --> [B, T, 1, 2]
-                pred = torch.argmax(out_fr, dim=3)
+                out_fr = net(img)  # [T, 1, 2]
 
                 l = nn.MSELoss()
-
-                loss = l(out_fr, label_onehot_shifted.float())
+                loss = l(out_fr, label_onehot.float())
 
                 test_samples += label.numel()
                 test_loss += loss.item() * label.numel()
-                test_acc += (pred == label_shifted.int()).float().sum().item()
-
-                test_f1 += f1_score(pred, label_shifted.int())
-                i += 1
+                test_acc += (out_fr.argmax(2) == label).float().sum().item()
+                if isinstance(test_f1_list, int):
+                    test_f1_list = out_fr.argmax(2).cpu()
+                    test_label_list = label.cpu()
+                else:
+                    test_f1_list = np.vstack((test_f1_list, out_fr.argmax(2).cpu()))
+                    test_label_list = np.vstack((test_label_list, label.cpu()))
                 functional.reset_net(net)
-        test_f1 /= i
+
+        test_f1 = f1(torch.Tensor(test_f1_list), torch.Tensor(test_label_list))
         test_loss /= test_samples
         test_acc /= test_samples
         print("Test", epoch, test_acc, test_f1)
+
         wandb.log({"train_acc": train_acc, "train_loss": train_loss, "train_f1": train_f1,
                    "test_acc": test_acc, "test_loss": test_loss, "test_f1": test_f1})
         if float(max_f1) < float(test_f1):
