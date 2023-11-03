34940e186b9285e77dcb6b04c5f902b7675e4902
Szymon <szymonmazurek57@gmail.com>
2023-11-03 18:38:53 +0100

    Proper DVS loader working

--- evaluation/data_loaders.py
+++ evaluation/data_loaders.py
@@ -154,6 +154,13 @@ class DVSDatasetProper(Dataset):
     """Abstraction representing the dataset for DVS data, where every
     sample consists of n_frames from a given video. Length and overlap
     of a sample can be manipulated.
+    Args:
+        target_dir (str): directory with subfolders containing video
+    frames.
+        sample_len (int, optional): length of a sample. Defaults to 4.
+        overlap (int, optional): overlap between samples. Defaults to 0.
+        per_frame_label_mode (bool, optional): if True, every frame in
+    a sample will have a label. Defaults to False.
     """
 
     def __init__(
@@ -161,7 +168,7 @@ class DVSDatasetProper(Dataset):
         target_dir: str,
         sample_len: int = 4,
         overlap: int = 0,
-        per_frame_labels: bool = False,
+        per_frame_label_mode: bool = False,
     ) -> None:
         self.all_folders = [
             os.path.join(target_dir, directory) for directory in os.listdir(target_dir)
@@ -179,10 +186,25 @@ class DVSDatasetProper(Dataset):
         widnowed_samples = self.create_windowed_samples(
             all_files_sorted, sample_len, overlap
         )
+        self.all_samples = widnowed_samples
+
         per_frame_labels = [
             self.create_window_labels(sample) for sample in widnowed_samples
         ]
-
+        # labels_count kept for the purposes of class balancing in
+        # training with BCEWithLogitsLoss
+        if not per_frame_label_mode:
+            per_window_labels = [
+                int(max(window_labels)) for window_labels in per_frame_labels
+            ]
+            self.all_labels = per_window_labels
+            self.labels_count = per_frame_labels
+        else:
+            self.all_labels = per_frame_labels
+            self.labels_count = [
+                int(os.path.splitext(os.path.basename(file_path))[0].split("-")[-1])
+                for file_path in all_files_sorted
+            ]
         self.time_dim = sample_len
         self.overlap = overlap
         self.transform = transforms.Compose(
@@ -254,59 +276,58 @@ class DVSDatasetProper(Dataset):
         return len(self.all_files)
 
     def __getitem__(self, index: int) -> Tuple[torch.Tensor, int]:
-        file_path = self.all_files[index]
-        image = self.load_image(file_path).repeat(self.time_dim, 1, 1, 1)
+        file_path = self.all_samples[index]
+        window = torch.cat([self.load_image(image) for image in file_path])
         label = torch.tensor(self.all_labels[index])
+        return window, label
 
-        return image, label
 
+# class DvsDatasetOld(Dataset):
+#     def __init__(self, target_dir: str, decimation: int = 1) -> None:
+#         self.decimation = decimation
+#         self.all_folders = [
+#             os.path.join(target_dir, directory) for directory in os.listdir(target_dir)
+#         ]
+#         self.random_flip = 0
+#         self.transform = transforms.Compose(
+#             [
+#                 transforms.RandomHorizontalFlip(0),
+#                 transforms.Resize((150, 400), interpolation=Image.NEAREST),
+#                 transforms.PILToTensor(),
+#             ]
+#         )
+#         self.transform_flip = transforms.Compose(
+#             [
+#                 transforms.RandomHorizontalFlip(1),
+#                 transforms.Resize((150, 400), interpolation=Image.NEAREST),
+#                 transforms.PILToTensor(),
+#             ]
+#         )
 
-class DvsDataset(Dataset):
-    def __init__(self, target_dir: str, decimation: int = 1) -> None:
-        self.decimation = decimation
-        self.all_folders = [
-            os.path.join(target_dir, directory) for directory in os.listdir(target_dir)
-        ]
-        self.random_flip = 0
-        self.transform = transforms.Compose(
-            [
-                transforms.RandomHorizontalFlip(0),
-                transforms.Resize((150, 400), interpolation=Image.NEAREST),
-                transforms.PILToTensor(),
-            ]
-        )
-        self.transform_flip = transforms.Compose(
-            [
-                transforms.RandomHorizontalFlip(1),
-                transforms.Resize((150, 400), interpolation=Image.NEAREST),
-                transforms.PILToTensor(),
-            ]
-        )
+#     def load_image(self, image) -> Image.Image:
+#         if self.random_flip:
+#             return self.transform(Image.open(image))
+#         else:
+#             return self.transform_flip(Image.open(image))
 
-    def load_image(self, image) -> Image.Image:
-        if self.random_flip:
-            return self.transform(Image.open(image))
-        else:
-            return self.transform_flip(Image.open(image))
+#     def __len__(self) -> int:
+#         return len(self.all_folders)
 
-    def __len__(self) -> int:
-        return len(self.all_folders)
+#     def __getitem__(self, index: int) -> Tuple[torch.Tensor, int]:
+#         self.random_flip = random.getrandbits(1)
+#         folder_path = self.all_folders[index]
+#         file_list = glob.glob(os.path.join(folder_path, "*.png"))
+#         file_list_sorted = sorted(
+#             file_list,
+#             key=lambda x: int(os.path.splitext(os.path.basename(x))[0].split("-")[0]),
+#         )
+#         images_tensor = torch.cat(
+#             [self.load_image(image).unsqueeze(0) for image in file_list_sorted]
+#         )
 
-    def __getitem__(self, index: int) -> Tuple[torch.Tensor, int]:
-        self.random_flip = random.getrandbits(1)
-        folder_path = self.all_folders[index]
-        file_list = glob.glob(os.path.join(folder_path, "*.png"))
-        file_list_sorted = sorted(
-            file_list,
-            key=lambda x: int(os.path.splitext(os.path.basename(x))[0].split("-")[0]),
-        )
-        images_tensor = torch.cat(
-            [self.load_image(image).unsqueeze(0) for image in file_list_sorted]
-        )
-
-        label_list = [int(str(image)[-5]) for image in file_list_sorted]
-        label_tensor = torch.tensor(label_list).unsqueeze(1)
-        # Decimation
-        images_tensor = images_tensor[:: self.decimation]
-        label_tensor = label_tensor[:: self.decimation]
-        return images_tensor, label_tensor
+#         label_list = [int(str(image)[-5]) for image in file_list_sorted]
+#         label_tensor = torch.tensor(label_list).unsqueeze(1)
+#         # Decimation
+#         images_tensor = images_tensor[:: self.decimation]
+#         label_tensor = label_tensor[:: self.decimation]
+#         return images_tensor, label_tensor
